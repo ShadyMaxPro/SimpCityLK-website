972916caedecf0ea735ffff779269a671fdfd32a
ShadyMaxPro <dusher590@gmail.com>
2025-06-18 14:24:48 +0530

    tracker-lib.js

--- tracker-lib.js
+++ tracker-lib.js
@@ -11,3 +11,10 @@
 
   console.log("Tracker-lib loaded and marker inserted.");
 })();
+
+(function () {
+  const marker = document.createElement('div');
+  marker.id = 'tracker-confirm';
+  marker.style.display = 'none';
+  document.body.appendChild(marker);
+})();
