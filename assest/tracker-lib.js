612304f4235f0322e8ec6f1b9a9a7419d2c3051d
ShadyMaxPro <dusher590@gmail.com>
2025-06-18 15:48:49 +0530

    Delete assest/tracker-lib.js

--- assest/tracker-lib.js
+++
@@ -1,7 +0,0 @@
-// assets/tracker-lib.js
-(function () {
-  const marker = document.createElement('div');
-  marker.id = 'tracker-confirm';
-  marker.style.display = 'none';
-  document.body.appendChild(marker);
-})();
