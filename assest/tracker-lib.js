097bd9ca581197ee75c96d7b09b2808a6008911d
ShadyMaxPro <dusher590@gmail.com>
2025-06-18 14:40:47 +0530

    Add files via upload

---
+++ assest/tracker-lib.js
@@ -0,0 +1,7 @@
+// assets/tracker-lib.js
+(function () {
+  const marker = document.createElement('div');
+  marker.id = 'tracker-confirm';
+  marker.style.display = 'none';
+  document.body.appendChild(marker);
+})();
